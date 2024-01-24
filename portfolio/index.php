6b9994816ce5f252908be8aac0531cd29054d68c
Lars Kuijer <lars.kuijer@student.nhlstenden.com>
2024-01-24 17:31:28 +0100

    Portfolio update

--- portfolio/index.php
+++ portfolio/index.php
@@ -15,16 +15,22 @@
     ?>
     <div class="container">
         <div class="portfolio-item">
-            <a href="portfolioItems/Feedback Lars Kuijer Portfolio.docx"><img src="images/feedback-1.jpg" alt="feedback"></a>
+            <a href="portfolioItems/Feedback Lars Kuijer Portfolio.docx"><img src="images/feedback-1.jpg" alt="feedback"></a><p>Feedback</p>
         </div>
         <div class="portfolio-item">
-            <a href="portfolioItems/Plan van aanpak M3T.docx"><img src="images/PLAN-VAN-AANPAK-320x320.jpg" alt="plan van aanpak"></a>
+            <a href="portfolioItems/Plan van aanpak M3T.docx"><img src="images/PLAN-VAN-AANPAK-320x320.jpg" alt="plan van aanpak"></a><p>Plan van aanpak</p>
         </div>
         <div class="portfolio-item">
-            <a href="portfolioItems/Vergadertechnieken"><img src="images/Vergadering.jpg" alt="plan van aanpak"></a>
+            <a href="portfolioItems/Vergadertechnieken Lars Kuijer.zip"><img src="images/Vergadering.jpg" alt="vergadertechnieken"></a><p>Vergadertechnieken</p>
         </div>
         <div class="portfolio-item">
-            <a href="portfolioItems/Vergadertechnieken"><img src="images/Presenteren-kun-je-leren.jpg" alt="plan van aanpak"></a>
+            <a href="portfolioItems/Presenteren Lars Kuijer.docx"><img src="images/Presenteren-kun-je-leren.jpg" alt="presenteren"></a><p>Presenteren</p>
+        </div>
+        <div class="portfolio-item">
+            <a href="portfolioItems/Edumundo opdrachten Lars Kuijer.docx"><img src="images/ED_Logo2019.png" alt="edumundo"></a><p>Edumundo</p>
+        </div>
+        <div class="portfolio-item">
+            <a href="portfolioItems/Reflectieverslag Lars Kuijer.docx"><img src="images/reflectie.jpg" alt="reflectieverslag"></a><p>Reflectieverslag</p>
         </div>
     </div>
     <footer>
